aee173ea8b130554d2b56aa8c252fad0475b783d
Mohammed Imran Hossain <imran09049@gmail.com>
2019-10-20 15:40:36 +0600

    Setup project user interface.

--- onlineShopping/src/main/java/com/imran/onlineShopping/controller/PageController.java
+++ onlineShopping/src/main/java/com/imran/onlineShopping/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.imran.onlineShopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,6 +12,14 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("welcomeMessage", "Hello!! Mohammed Imran Hossain welcome to Spring MVC world.");
+		return mv;
+	}
+	
+	
+	@RequestMapping(value = "/test/{welcomeMessage}")
+	public ModelAndView test(@PathVariable(value = "welcomeMessage", required = false) String welcomeMessage) {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("welcomeMessage", welcomeMessage.isEmpty() ? "Hello There" : welcomeMessage);
 		return mv;
 	}
 }
